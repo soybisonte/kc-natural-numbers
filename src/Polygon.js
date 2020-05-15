154087bd9576637b5b624bdbd6117d813c7d67f5
Hector Heriberto  Rodriguez Martinez <soybisonte@MacBook-Pro-de-Hector.local>
2020-05-14 20:23:20 -0500

    complete

--- src/Polygon.js
+++ src/Polygon.js
@@ -1,11 +1,23 @@
 import * as p5 from 'p5'
 
 export default class Polygon {
-    constructor(p5) {
-        this.p5 =  p5
+    constructor(p5, options) {
+        this.p5 =  p5;
+        this.apothema = options.apothema;
+        this.edges = options.edges;
+        this.center = options.center;
     }
     render () {
-        const { p5 } = this
-        p5.ellipse(0, 0, 100, 100);
+        const { p5, apothema, edges, center } = this
+        // p5.ellipse(0, 0, 100, 100);
+        let dA = 2 * Math.PI / edges;
+        
+        p5.beginShape();
+            for(var i = 0; i < edges; i++) {
+                let x = apothema * Math.cos(dA * i) + center.x;
+                let y = apothema * Math.sin(dA * i) + center.y;
+                p5.vertex(x, y);
+            }
+        p5.endShape(p5.CLOSE);
     }
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Polygon from './Polygon'
 
 const s = (p5) => {
   
+  
 
   p5.setup = () => {
     p5.createCanvas(500, 500);
@@ -14,24 +15,33 @@ const s = (p5) => {
     p5.background(0);
 
     const size = 50;
-    for(let i = 0; i < 25; i++) {
+    for(let i = 0; i < 11; i++) {
       
-      for (let j = 0; j < 25; j++) {
+      for (let j = 0; j < 11; j++) {
+        let poly = new Polygon(p5, {
+          apothema: 35,
+          edges: 6,
+          center: { x: size / 2, y: size / 2 }
+        });
         // malla
         // p5.noFill();
         // p5.stroke(255)
         // p5.rect(i * size, j * size, size, size);
+        // p5.fill(255)
         // p5.noStroke()
-        
-        p5.push()
-          p5.translate(i * size , j * size );  
-          p5.fill(255);
-          p5.ellipse(size / 2, size / 2, size, size)
-          p5.fill(0);
-          p5.ellipse(size / 2, size / 2, 0.6 * size, 0.6 * size)
-          p5.fill(255);
-        p5.ellipse(size / 2, size / 2, 0.3 * size, 0.3 * size)
-        p5.pop()
+          p5.push()
+            p5.translate(i * size, j * size);
+            p5.rotate(Math.PI / 2)
+            p5.fill(255);
+            // poly.apothema = 25;
+            poly.render();
+            // p5.fill(255);
+            // p5.ellipse(size / 2, size / 2, size, size)
+            // p5.fill(0);
+            // p5.ellipse(size / 2, size / 2, 0.6 * size, 0.6 * size)
+            // p5.fill(255);
+            // p5.ellipse(size / 2, size / 2, 0.3 * size, 0.3 * size)
+          p5.pop()
       }
     }
   };
